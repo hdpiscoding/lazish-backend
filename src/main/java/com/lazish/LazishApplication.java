3f8ef1abfc03770ab2b7132250d2991fe7f7140c
hdpiscoding <22520339@gm.uit.edu.vn>
2025-02-24 21:56:07 +0700

    initial push

---
+++ src/main/java/com/lazish/LazishApplication.java
@@ -0,0 +1,13 @@
+package com.lazish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LazishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LazishApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lazish/LazishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lazish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LazishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
